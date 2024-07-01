8420b40bdd8012d4e15b5308f2902febb4a41fa0
archit1210 <architlokhande72@gmail.com>
2024-07-01 21:32:03 +0530

    Integrated coingecko API

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,78 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
+
+const Home = () => {
+  const { allcoins, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+  const [input, setInput] = useState("");
+
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    const filteredCoins = allcoins.filter((item) =>
+      item.name.toLowerCase().includes(input.toLowerCase())
+    );
+    setDisplayCoin(filteredCoins);
+  };
+
+  useEffect(() => {
+    setDisplayCoin(allcoins);
+  }, [allcoins]);
+
+  return (
+    <div className="home">
+      <div className="hero">
+        <h1>
+          Largest <br /> Crypto Marketplace
+        </h1>
+        <p>Welcome to the world's largest cryptocurrency Marketplace</p>
+        <form onSubmit={searchHandler}>
+          <input
+            onChange={inputHandler}
+            value={input}
+            type="text"
+            placeholder="Search Crypto...."
+            required
+          />
+          <button>SEARCH</button>
+        </form>
+      </div>
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{ textAlign: "center" }}>24 Hour change</p>
+          <p className="market-cap">Market Cap</p>
+        </div>
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div className="table-layout" key={index}>
+            <p>{item.market_cap_rank}</p>
+            <div className="">
+              <img src={item.image} alt="" />
+              <p>{item.name + " - " + item.symbol}</p>
+            </div>
+            <p>
+              {currency.symbol} {item.current_price.toLocaleString()}
+            </p>
+            <p
+              className={item.price_change_percentage_24h > 0 ? "green" : "red"}
+            >
+              {Math.floor(item.price_change_percentage_24h * 100) / 100}%
+            </p>
+            <p className="market-cap">
+              {currency.symbol}
+              {item.market_cap.toLocaleString()}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
