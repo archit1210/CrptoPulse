91f8a0c6511bcc4dd18ce2d8b21a4f02a0cd1faf
archit1210 <architlokhande72@gmail.com>
2024-07-01 21:32:25 +0530

    Completed footer and search function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 import arrow_icon from "../../assets/arrow_icon.png";
+import { CoinContext } from "../../context/CoinContext";
+
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+  const currencyHandler = (e) => {
+    switch (e.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
   return (
     <div className="navbar">
-      <img src={logo} alt="" className="logo" />
+      <Link to={"/"}>
+        <img src={logo} alt="" className="logo" />
+      </Link>
       <ul>
         <li>HOME</li>
         <li>FEATURES</li>
@@ -14,7 +40,7 @@ const Navbar = () => {
       </ul>
       <div className="nav-right">
         <div>
-          <select name="" id="">
+          <select name="" id="" onChange={currencyHandler}>
             <option value="usd">USD</option>
             <option value="inr">INR</option>
             <option value="eur">EUR</option>
